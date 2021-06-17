33f7a3557c082a6c81bbeb9de53c9c011692155c
Gharbaoui <mohamed9gharbaoui@gmail.com>
2021-06-17 14:33:59 +0100

    done

--- NORMED/he_mult_pipe.c
+++
@@ -1,7 +0,0 @@
-#include "minishell.h"
-
-void	default_fds(int *stdo, int *stdi)
-{
-	*stdi = dup(0);
-	*stdo = dup(1);
-}
--- dec.vim
+++
@@ -1,7 +0,0 @@
-let @a=""
-g/^void/y A
-g/^char/y A
-g/^int/y A
-g/^t_/y A
-call feedkeys("\<CR>")
-q
